957bae899ecf360611c3d60b020f4efe1d3a1262
Daniel Genov <danigenov@protonmail.com>
2025-12-23 02:18:51 +0100

    feat : add settings for creating table row

--- src/renderCreateGame.js
+++ src/renderCreateGame.js
@@ -27,4 +27,44 @@ export function renderCreateGame() {
   const settingsWrapperDOM = document.createElement('div');
   settingsWrapperDOM.id = 'settingsWrapper_id';
   root.appendChild(settingsWrapperDOM);
+
+  // INDIVIDUAL SETTINGS
+  const numberPlayersDOM = document.createElement('div');
+  numberPlayersDOM.id = 'numberPlayers_id';
+  numberPlayersDOM.className = 'individualSettingDOM';
+  numberPlayersDOM.textContent = 'Jugadores';
+  settingsWrapperDOM.appendChild(numberPlayersDOM);
+
+  // SELECTOR
+
+  const numberPlayersSelectorDOM = document.createElement('div');
+  numberPlayersSelectorDOM.id = 'numberPlayersSelector_id';
+  numberPlayersSelectorDOM.className = 'selectorDOM';
+  numberPlayersDOM.appendChild(numberPlayersSelectorDOM);
+
+  // SELECTOR '<'
+  const numberPlayersSelectorLessDOM = document.createElement('button');
+  numberPlayersSelectorLessDOM.id = 'numberPlayersSelectorLess_id';
+  numberPlayersSelectorLessDOM.textContent = '<';
+  numberPlayersSelectorDOM.appendChild(numberPlayersSelectorLessDOM);
+
+  // ACTUAL SELECTOR COUNT
+  let numberPlayersSelectorCount = 0;
+  const numberPlayersSelectorCountDOM = document.createElement('div');
+  numberPlayersSelectorCountDOM.id = 'numberPlayersSelectorCount_id';
+  numberPlayersSelectorCountDOM.textContent = `${numberPlayersSelectorCount}`;
+  numberPlayersSelectorDOM.appendChild(numberPlayersSelectorCountDOM);
+
+  // SELECTOR '>'
+  const numberPlayersSelectorMoreDOM = document.createElement('button');
+  numberPlayersSelectorMoreDOM.id = 'numberPlayersSelectorMore_id';
+  numberPlayersSelectorMoreDOM.textContent = '>';
+  numberPlayersSelectorDOM.appendChild(numberPlayersSelectorMoreDOM);
+
+  // INDIVIDUAL SETTING
+  const numberImpostorsDOM = document.createElement('div');
+  numberImpostorsDOM.id = 'numberImpostors_id';
+  numberImpostorsDOM.className = 'individualSettingDOM';
+  numberImpostorsDOM.textContent = 'Impostores';
+  settingsWrapperDOM.appendChild(numberImpostorsDOM);
 }
